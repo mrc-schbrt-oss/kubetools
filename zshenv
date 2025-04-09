2788f8a79fb267493c3a8d78047d9d58295fb92d
Marco Schubert <marco.schubert@sva.de>
2025-04-09 16:47:04 +0200

    initial commit

---
+++ zshenv
@@ -0,0 +1,16 @@
+# System-wide .zshenv file for zsh(1), sourced on all invocations of the shell.
+#
+# Global order: zshenv, zprofile, zshrc, zlogin
+
+export ZDOTDIR=/root/data
+
+
+# If $ZDOTDIR is not set and none of .zshenv, .zprofile, .zshrc, .zlogin exist
+# in $HOME, read ZSH startup files from $XDG_CONFIG_HOME/zsh/ instead of $HOME. # The point is to promote XDG-based location, but don't break existing setups.
+if [[ -z "${ZDOTDIR-}" ]] && _x=("$HOME"/.z{shenv,profile,shrc,login}(N)) && (( ! $#_x ));    then
+    ZDOTDIR=${XDG_CONFIG_HOME:-$HOME/.config}/zsh
+fi
+unset _x
+
+
+##Hallo
---
+++ zshrc-default.zsh
@@ -0,0 +1,33 @@
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
+export ZSH=/usr/share/oh-my-zsh
+ZSH_THEME="robbyrussell"
+
+plugins=(git)
+
+source $ZSH/oh-my-zsh.sh
+
+[[ ! -f ~/data/.p10k.zsh ]] || source ~/data/.p10k.zsh
+source <(kubectl completion zsh)
+alias k=kubectl
+alias kx=kubectx
+alias kn=kubens
+alias kd=kubectl-view_secret
+alias klogs=kubectl-stern
+alias kimages=kubectl-images
+alias ktop=kubectl-ktop
+alias kstatus=kubectl-status
+alias kview-np=kubectl-np_viewer
+alias koutdated=kubectl-outdated
+alias krbac=kubectl-rbac_tool
+alias ksick=kubectl-sick_pods
+alias kview-alloc=kubectl-view_allocations
+alias kview-quotas=kubectl-view_quotas
+alias kvirt=kubectl-virt
+export KUBECONFIG=/root/data/.kube/kubeconfig.yaml
+export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
+export SSH_CONFIG_FILE=~/data/.ssh/config
+export HISTFILE=~/data/.zsh_history
+export BYOBU_CONFIG_DIR=/root/data/.byobu
