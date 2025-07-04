82b12a06c9995b2faa7b0d708738b64c66b360c4
mrc-schbrt-oss <75045199+mrc-schbrt-oss@users.noreply.github.com>
2025-07-04 21:58:20 +0200

    Update zshrc-default.zsh

--- zshrc-default.zsh
+++ zshrc-default.zsh
@@ -12,22 +12,14 @@ source $ZSH/oh-my-zsh.sh
 [[ ! -f ~/data/.p10k.zsh ]] || source ~/data/.p10k.zsh
 source <(kubectl completion zsh)
 source <(flux completion zsh)
+source <(argocd completion zsh)
+source <(helm completion zsh)
+source <(cilium completion zsh)
 
 alias k=kubectl
 alias kx=kubectx
 alias kn=kubens
 alias kd=kubectl-view_secret
-alias klogs=kubectl-stern
-alias kimages=kubectl-images
-alias ktop=kubectl-ktop
-alias kstatus=kubectl-status
-alias kview-np=kubectl-np_viewer
-alias koutdated=kubectl-outdated
-alias krbac=kubectl-rbac_tool
-alias ksick=kubectl-sick_pods
-alias kview-alloc=kubectl-view_allocations
-alias kview-quotas=kubectl-view_quotas
-alias kvirt=kubectl-virt
 
 
 rm -f /root/data/.kube/kubeconfig-flatten.yaml
